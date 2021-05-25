d4b26c24160fd84854f7199b1587eb0b0e3f8b91
Pasha Palangpour <pashapal@gmail.com>
2021-05-24 22:28:24 -0500

    Add geolocation hook.
    A custom hook for grabbing geolocation from the browser may be an
    unnecessary abstraction at this point... But it works for now.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-function App() {
+import React from 'react';
+import usePosition from './components/GeoLoc';
+
+const App = () => {
+  const { position, error } = usePosition();
+  console.log(position);
+  if (error != null) {
+    console.log(error);
+  }
+
   return (
     <div className="App">
+      {error ? (
+        <h4>Error loading geolocation</h4>
+      ) : position ? (
+        <h4>{position.latitude} {position.longitude}</h4>
+      ) : (
+        <h4>Loading geolocation</h4>
+      )}
     </div>
   );
+
 }
 
 export default App;
