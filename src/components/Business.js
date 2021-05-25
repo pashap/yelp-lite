3b274f35fcf8d3bea7f76d2b0c42b1c1b37f5b5a
Pasha Palangpour <pashapal@gmail.com>
2021-05-24 23:09:02 -0500

    Add detailed view of a Business.

--- src/components/Business.js
+++ src/components/Business.js
@@ -1,19 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const Business = ({ business }) => {
-  return (
-    <div className="business">
-      <h2>{business.name}</h2>
-      <div>
-        <img
-          width="200"
-          alt={`The business is rated: ${business.rating}`}
-          src={business.image_url}
-        />
-      </div>
-    </div>
-  );
+    const [detailed, setDetailed] = useState(false);
+
+    const categories = business.categories.map(cat => cat.title);
+    return (
+        <div className="business">
+            <h2>{business.name}</h2>
+            <div>
+                <img
+                    width="200"
+                    alt={`The business is rated: ${business.rating}`}
+                    src={business.image_url}
+                />
+            </div>
+            <button onClick={() => setDetailed(!detailed)}> Details</button>
+            {detailed && (
+                <div>
+                    <p>Price: {business.price}</p>
+                    <p>Rating: {business.rating}</p>
+                    <p>Transactions: {business.transactions.join(' ')}</p>
+                    <p>Categories: {categories.join(' ')}</p>
+                </div>
+            )}
+        </div>
+    );
 };
 
 
